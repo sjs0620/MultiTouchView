322740824ef87f4e18d75552845414efe598d5c7
신재순 <sjslsm@gmail.com>
2018-10-23 13:58:52 +0900

    Initial commit

---
+++ app/src/main/java/com/sjs/edu/multitouchview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sjs.edu.multitouchview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MultiTouchView(this,null));
+    }
+}
---
+++ app/src/main/java/com/sjs/edu/multitouchview/MultiTouchView.java
@@ -0,0 +1,63 @@
+package com.sjs.edu.multitouchview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.jar.Attributes;
+
+public class MultiTouchView extends View {
+    final  int MAX_POINTS = 10;
+    float[] x = new float[ MAX_POINTS];
+    float[] y = new float[ MAX_POINTS];
+    boolean[] touching = new boolean[ MAX_POINTS];
+    Paint mPaint;
+
+    public  MultiTouchView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for(int i=0; i<MAX_POINTS; i++){
+            if(touching[i]){
+                canvas.drawCircle(x[i], y[i],60,mPaint);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int index = event.getActionIndex();
+        int id = event.getPointerId(index);
+        int action = event.getActionMasked(); //up/down/move...
+
+        switch (action){
+            case MotionEvent.ACTION_DOWN: break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                x[id] = (int)event.getX(index);
+                y[id] = (int)event.getY(index);
+                touching[id] = true;
+                break;
+            case MotionEvent.ACTION_MOVE: break;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_CANCEL:
+               touching[id] = false;
+                break;
+        }
+
+
+        invalidate(); //onDraw()호출
+        return true;
+    }
+}
